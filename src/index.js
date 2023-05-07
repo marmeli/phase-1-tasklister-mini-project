7b5d80b552cd4bf0d1200fd79a18508747b0c9a1
melissamariano <melissams538@gmail.com>
2023-05-07 16:05:58 -0400

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners()
 });
+function addingEventListerners() {
+  document
+  .getElementById("create-task-form")
+  .addEventListeners("submit", handleFormSubmit)
+
+}
+
+function handleFormSubmit(e){
+  e.preventDefualt()
+  // console.log(e)
+  const task = e.target[0].value
+  // 
+  console.log(task)
+
+  displayTask(task)
+}
+
+function displayTask(task){
+
+const taskUl = document.getElementById("tasks")
+// console.log(taskUl)
+const taskLi = document.createElement("li")
+
+const deleteBtn = document.createElement("button")
+deleteBtn.textcontent = "x"
+deleteBtn.addEventListener("click", deleteTask)
+
+taskLi.textcontent = task + " "
+taskLi.appendChild(deleteBtn)
+taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+  console.log(e)
+  e.target.parentNode.remove()
+}
